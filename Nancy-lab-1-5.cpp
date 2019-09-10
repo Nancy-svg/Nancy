56a13af771158bdacb4e4913e043e08af4babfcc
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-10 20:01:33 +0300

    Add files via upload

---
+++ Nancy-lab-1-5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>  
+#include <cmath>
+using namespace std;
+int main()
+{
+	
+	float a, b, result ,abs_a, abs_b;
+	cout << "Enter a" << endl;
+	cin >> a;
+	abs_a = abs(a);
+	cout << "Enter b" << endl;
+	cin >> b;
+	abs_b = abs(b);
+	result = abs_a + abs_b;
+	cout << "Summa modyleu" << " "<< result << endl;
+	result = abs_a - abs_b;
+	cout << "Raznost moduleu" << " " << result << endl;
+	result = abs_a * abs_b;
+	cout << "Proizvedenie moduleu" << " " << result << endl;
+	result = abs_a / abs_b;
+	cout << "Chastnoe moduleu" << " " << result << endl;
+	return 0;
+}
