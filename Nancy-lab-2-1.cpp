568bb58b72d237a8795754f6a0165517a5fbb6ff
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-10 21:28:27 +0300

    Add files via upload

---
+++ Nancy-lab-2-1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	float x1, x2, y1, y2, result;
+	cout << "Enter x1 and x2" << endl;
+	cin >> x1 >> x2;
+	cout << "Enter y1 and y2" << endl;
+	cin >> y1 >> y2;
+	result = sqrt((x2-x1) * (x2 - x1) + (y2-y1) * (y2 - y1)) ;
+	cout << "Result is" << result << endl;
+    return 0;
+}
---
+++ Nancy-lab-2-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, result, result_1;
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter B" << endl;
+	cin >> b;
+	cout << "Enter C" << endl;
+	cin >> c;
+	result = abs(b - a);
+	cout << "AB=" << result << endl;
+	result_1 = abs(c - b);
+	cout << "BC=" << result_1 << endl;
+	result = result + result_1;
+	cout << "AB+BC=" << result << endl;
+
+	return 0;
+}
