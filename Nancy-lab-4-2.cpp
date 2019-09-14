c614c7c734f28847798497359688853d33a1184e
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-14 09:39:48 +0300

    Add files via upload

---
+++ Nancy-lab-4-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+	const double pi = 3.14;
+	double a, result;
+	cout << "Enter A (0 < A < 360)" << endl;
+	cin >> a;
+	result = 180 * a / pi;
+	cout << "radian=" << result << endl;
+
+	return 0;
+
+}
