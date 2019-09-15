f45d827e2a6153f14a0aa80986b6ea8539ec2fc7
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-15 19:12:14 +0300

    Add files via upload

---
+++ Nancy-lab-6-5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+	int got, k1, k2, result;
+	cout << "Enter got" << endl;
+	cin >> got;
+
+	k1 = got / 100;
+	k2 = got % 100;
+	if (k2 > 0)
+	{
+		result = k1 + 1;
+		cout << "century is : " << result << endl;
+	}
+	else {
+		result = k1;
+		cout << "century is : " << result << endl;
+	}
+
+	return 0;
+}
+
+
+	
