ca4530e52ae6f536e8b99f0471772b4b8d438e66
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-24 18:16:09 +0300

    Update Nancy-lab-2-1.cpp

--- Nancy-lab-2-1.cpp
+++ Nancy-lab-2-1.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	float x1, x2, y1, y2, result;
-	cout << "Enter x1 and x2" << endl;
+	cout << "Enter (x1;y1)" << endl;
 	cin >> x1 >> x2;
-	cout << "Enter y1 and y2" << endl;
+	cout << "Enter (x2;y2)" << endl;
 	cin >> y1 >> y2;
 	result = sqrt((x2-x1) * (x2 - x1) + (y2-y1) * (y2 - y1)) ;
 	cout << "Result is" << result << endl;
