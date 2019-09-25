0234db2f0b981260b2a840c30bf662914e0b11a7
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-25 09:54:35 +0300

    Add files via upload

---
+++ Nancy-lab-3-2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+	int a, b,c, k;
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter B" << endl;
+	cin >> b;
+	cout << "Enter C" << endl;
+	cin >> c;
+	k = a;
+	a = b;
+	b = c;
+	c = k;
+	cout << "A = " << a << endl;
+	cout << "B = " << b << endl;
+	cout << "C = " << c << endl;
+	return 0;
+}
