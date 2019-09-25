db3fb4ef82e662f6a97f1b64c0af77717ccb81a5
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-25 11:25:18 +0300

    Add files via upload

---
+++ Nancy-lab-3-6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	float a, k;
+	cout << "Enter a" << endl;
+	cin >> a;
+	a = a * a;
+	k = a * a;
+	k = k * k;
+	cout << "resuly is : " << k << endl;
+   return 0;
+}
---
+++ Nancy-lab-3-7.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	float a, b, c;
+	cout << "Enter a" << endl;
+	cin >> a;
+	b = a * a;
+	c = b * a;
+	a = b * c;
+	a = a * a * a;
+	cout << "Result is : " << a << endl;
+
+	return 0;
+}
