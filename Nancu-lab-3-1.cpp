78a196536f2e7dc8c1236a9c1510eccd520fbbe5
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-25 09:41:28 +0300

    Add files via upload

---
+++ Nancu-lab-3-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+	int a, b, k;
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter B" << endl;
+	cin >> b;
+
+	k = a;
+	a = b;
+	b = k;
+	cout << "A = " << a << endl;
+	cout << "B = " << b << endl;
+
+	return 0;
+}
