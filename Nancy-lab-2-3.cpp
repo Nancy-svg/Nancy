310c08b78634f90db9803faec7f2e37558d98d28
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-09-25 11:09:07 +0300

    Add files via upload

---
+++ Nancy-lab-2-3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, result, result_1;
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter B" << endl;
+	cin >> b;
+	cout << "Enter C" << endl;
+	cin >> c;
+
+	result = abs(c - a);
+	result_1 = abs(c - b);
+	result = result * result_1;
+	cout << "AC*BC=" << result << endl;
+	return 0;
+}
