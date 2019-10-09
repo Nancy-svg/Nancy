12ef7fd3073c24eb50f74e96abb16b650b0ab36f
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-10-09 10:47:08 +0300

    Add files via upload

---
+++ Nancy-lab-11-1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, i, j, result;
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter B (A < B)" << endl;
+	cin >> b;
+
+	c = b - a -1;
+	a = a - 1;
+
+	for (i = 0; i <= c; i++) {
+		a++;
+		for (j = 0; j < a; j++) {
+			cout << a;
+		}
+		cout << " " << endl;
+	}
+
+	return 0;
+}
---
+++ Nancy-lab-11-2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, i, j, result;
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter B (A > B)" << endl;
+	cin >> b;
+	c = 0;
+	while (c < a - b) {
+		c = c + b;
+	}
+	result = a - c;
+	cout << "Result is " << result << endl;
+	return 0;
+}
