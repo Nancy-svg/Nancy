9f1df4524d4699c0bf9769d877948f9dab0ed7ca
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-10-16 11:56:34 +0300

    Add files via upload

---
+++ Nancy-lab-7-3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	
+
+
+	{
+		cout << "Double inequality is true" << endl;
+	}
+	else {
+		cout << "double inequality is wrong" << endl;
+	}
+
+	return 0;
+}
---
+++ Nancy-lab-7-5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>   
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c, d;
+	cout << "Ведите четырехзначное число" << endl;
+	cin >> a;
+
+	b = a / 100;
+	a = a % 100;
+	c = a / 10;
+	d = a % 10;
+	c = d * 10 + c;
+
+	if (b == c) cout << "Число является полиндромом";
+	else cout << "Число не является полиндромом";
+	return 0;
+}
