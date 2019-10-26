2bc7fc204898c2313bf04aa0b091e0a1bf4cf283
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-10-26 16:00:31 +0300

    Add files via upload

---
+++ Nancy-lab-14-1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a, i, n, k, l;
+	cout << "Enter N" << endl;
+	cin >> n;
+	cout << "Enter k and L (1<=k<=l<=n)" << endl;
+	cin >> k >> l;
+
+	int* array = new int[n];
+
+	for (i = 0;i < n;i++) {
+		cout << "Enter array[" << i + 1 << "] = ";
+		cin >> array[i];
+	}
+
+	for
