5695c4d2d99240591a974460e58350d177889c86
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-10-30 11:00:02 +0300

    Add files via upload

---
+++ Nancy-lab-8-6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a, k, g;
+	cout << "Enter a" << endl;
+	cin >> a;
+	if (a == 0) cout << "нулевое число" << endl;
+	else {
+		if (a < 0) cout << "отризацетльное число число" << endl;
+		else {
+			k = a % 2;
+			
+			if (k == 1) cout << "положительное не четно число" << endl;
+		}
+	}
+	g = a % 2;
+	if  (a / 100 = 2) cout << "четное двузначное число" << endl;
+	else { if (g == 1 && a / 100) cout << "число трезначное не четное" << endl; }
+	return 0;
+}
