e48c8a1d0d315394916a5fef938b544549eedb9b
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-11-08 10:53:34 +0300

    Add files via upload

---
+++ Nancy-lab-10-4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float a, n, i, result;
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter N (n > 0)" << endl;
+	cin >> n;
+	result = 0;
+	for (i = 0; i <= n;i++) {
+		result = result + pow(a, i);
+	}
+	cout << "Result is " << result << endl;
+	return 0;
+}
