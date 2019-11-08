0303646f9cd0b34ccf1c1a63963439414b97fbfd
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-11-08 11:39:27 +0300

    Update Nancy-lab-9-1.cpp

--- Nancy-lab-9-1.cpp
+++ Nancy-lab-9-1.cpp
@@ -1,20 +1,62 @@
-﻿// Nancy-lab-9-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
-
+#include <math.h>
+#include <conio.h>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+	int k, n, result;
+	cout << "Ведите день" << endl;
+	cin >> n;
+	cout << "Ведите месяц" << endl;
+	cin >> k;
+	switch (n) {
+	    case 1: cout << "первое " ;break;
+	    case 2: cout << "второе " ;break;
+	    case 3: cout << "третье " ;break;
+	    case 4: cout << "четвертое " ;break;
+	    case 5: cout << "пятое " ;break;
+	    case 6: cout << "шестое " ;break;
+	    case 7: cout << "седьмое " ;break;
+	    case 8: cout << "восьмое " ;break;
+	    case 9: cout << "девятое " ;break;
+	    case 10: cout << "десятое " ;break;
+	    case 11: cout << "одинадцатое " ;break;
+	    case 12: cout << "двенадцатое " ;break;
+	    case 13: cout << "тринадцатое " ;break;
+	    case 14: cout << "четырнадцатое" ;break;
+	    case 15: cout << "пятнадцатое " ;break;
+	    case 16: cout << "шестнадцатое " ;break;
+	    case 17: cout << "семнадцатое " ;break;
+	    case 18: cout << "восемьнадцатое " ;break;
+	    case 19: cout << "девятнадцатое " ;break;
+	    case 20: cout << "двадцатое " ;break;
+	    case 21: cout << "двадцать первое " ;break;
+	    case 22: cout << "двадцать второе " ;break;
+	    case 23: cout << "двадцать третье " ;break;
+	    case 24: cout << "двадцать четвертое " ;break;
+	    case 25: cout << "двадцать пятое " ;break;
+	    case 26: cout << "двадцать шестое " ;break;
+	    case 27: cout << "двадцать седьмое " ;break;
+	    case 28: cout << "двадцать восьмое " ;break;
+	    case 29: cout << "двадцать девятое " ;break;
+	    case 30: cout << "тридцатое " ;break;
+	    case 31: cout << "тридцать первое " ;break;
+	}
+	switch (k) {
+	    case 1: cout << "января " ;break;
+	    case 2: cout << "февраля " ;break;
+	    case 3: cout << "марта " ;break;
+	    case 4: cout << "апреля " ;break;
+	    case 5: cout << "мая " ;break;
+	    case 6: cout << "июня " ;break;
+	    case 7: cout << "июля " ;break;
+	    case 8: cout << "августа " ;break;
+	    case 9: cout << "сентября " ;break;
+	    case 10: cout << "октября " ;break;
+	    case 11: cout << "ноября " ;break;
+	    case 12: cout << "декабря " ;break;
+	}
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	   
