b8f864451a250d754d85de9199d9cc3e2e7963fd
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-11-16 15:26:16 +0300

    Add files via upload

---
+++ Nancy-lab-15-1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n, k, i;
+
+	cout << "Enter N" << endl;
+	cin >> n;
+	int* a = new int[n];
+	int* b = new int[n];
+	
+	for (i = 0;i < n;i++) {
+		cout << "Enter a[" << i + 1 << "] " ;
+		cin >> a[i];
+	}
+	for (i = 0;i < n;i++) {
+		cout << "Enter b[" << i + 1 << "] " ;
+		cin >> b[i];
+	}
+	cout << "Array A ";
+	for (i = 0;i < n;i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	cout << "Array B ";
+	for (i = 0;i < n;i++) {
+		cout << b[i] << " ";
+	}
+	cout << endl;
+
+	for (i = 0;i < n;i++) {
+		k = a[i];
+		a[i] = b[i];
+		b[i] = k;
+
+	}
+
+	cout << "Array A ";
+	for (i = 0;i < n;i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	cout << "Array B ";
+	for (i = 0;i < n;i++) {
+		cout << b[i] << " ";
+	}
+	return 0;
+}
+
+
+
