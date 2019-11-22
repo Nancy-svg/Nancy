1d25e3ec3fd0c2ea87789f162c364bf789f823ff
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-11-22 11:07:56 +0300

    Add files via upload

---
+++ Nancy-lab-13-3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a, d, i, n, result;
+	cout << "Enter N (n>2)" << endl;
+	cin >> n;
+	int* array = new int[n];
+	cout << "Enter A" << endl;
+	cin >> a;
+	cout << "Enter B" << endl;
+	cin >> d;
+	array[0] = a;
+	array[1] = d;
