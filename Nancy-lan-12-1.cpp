79f88a0688e206f7a304921c5dd11489d7e1a347
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-11-26 19:41:56 +0300

    Add files via upload

---
+++ Nancy-lan-12-1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>   
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a, b, i;
+	for (i = 0; i < 6; i++) {
+		cout << "Enter A ";
+		cin >> a;
+		b = pow(a, 3);
+		cout << "  B = " << b << endl;
+	}
+	return 0;
+}
---
+++ Nancy-lan-12-4.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream> 
+#include <cmath>
+
+	using namespace std;
+	int main()
+	{
+		setlocale(LC_ALL, "Russian");
+		int y, x, i;
+		for (i = 0;i < 3;i++) {
+			cout << "Enter (x,y)" << endl;
+			cin >> x >> y;
+
+			if (x > 0) {
+				if (y > 0) cout << "Первая четверть" << endl;
+				else cout << "четвертая четверть" << endl;
+			}
+			else {
+				if (y > 0) cout << "вторя четверть" << endl;
+				else cout << "третья четверть" << endl;
+			}
+		}
+	return 0;
+}
