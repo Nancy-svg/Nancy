fca386c0dd1cc5b0c047661755ee7d47365a3b5c
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-12-17 12:14:16 +0300

    Update Nancy-lab-12-3.cpp

--- Nancy-lab-12-3.cpp
+++ Nancy-lab-12-3.cpp
@@ -1,16 +1,16 @@
-﻿#include <iostream>   
+#include <iostream>   
 #include <cmath>
 using namespace std;
-
+float RingS(float r1 ,float r2)
+{
+	float result;
+	result = 3.14 * r1 * r1 - 3.14 * r2 * r2;
+	return result;
+}
 int main()
 {
-	int  i,  r1,r2;
-	float x;
-	for (i = 0;i < 3;i++) {
-		cout << "enter R1 and R2 (R1 > R2) ";
-			cin >> r1 >> r2;
-			x = 3.14 * r1 * r1 - 3.14 * r2 * r2;
-			cout << "Result is " << x << endl;
-	}
-	return 0;
+	float r1, r2;
+	cout << "enter R1 and R2 (R1 > R2) ";
+	cin >> r1 >> r2;
+	cout << "Result is " << RingS(r1 ,r2) << endl;
 }
