a1d948d266b5c406fc31ec14b5cba4ca856aa445
Нэнси <55014700+Nancy-svg@users.noreply.github.com>
2019-12-17 11:45:57 +0300

    Update Nancy-lab-12-1.cpp

--- Nancy-lab-12-1.cpp
+++ Nancy-lab-12-1.cpp
@@ -1,15 +1,17 @@
 ﻿#include <iostream>   
 #include <cmath>
 using namespace std;
-
+int power(int a)
+{
+	int b;
+	b = pow(a, 3);
+	return b;
+}
 int main()
 {
-	int a, b, i;
-	for (i = 0; i < 6; i++) {
-		cout << "Enter A ";
-		cin >> a;
-		b = pow(a, 3);
-		cout << "  B = " << b << endl;
-	}
+	int A;
+	cout << "Enter A" << endl;
+	cin >> A;
+	cout << "Result is " << power(A) << endl;
 	return 0;
 }
